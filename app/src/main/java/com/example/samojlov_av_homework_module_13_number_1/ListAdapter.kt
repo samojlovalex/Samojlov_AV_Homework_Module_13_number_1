dd17f95e4adc2f5916d3117fa87fe443ad335eef
Alexandr Samojlov <samojlovalex@gmail.com>
2024-09-12 00:41:00 +0300

    Закончил приложение

--- app/src/main/java/com/example/samojlov_av_homework_module_13_number_1/ListAdapter.kt
+++ app/src/main/java/com/example/samojlov_av_homework_module_13_number_1/ListAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.samojlov_av_homework_module_13_number_1
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import android.widget.TextView
 
 class ListAdapter(context: Context, listItem: MutableList<String>) :
     ArrayAdapter<String>(context, R.layout.list_item, listItem) {
+    @SuppressLint("SetTextI18n")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var view = convertView
         val stroke: String? = getItem(position)
